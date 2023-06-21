ffc4ac6c24d70b299e0e0ff53ff8a835462045ed
Cristian <cristianald00@hotmail.com>
2023-06-20 23:55:21 -0400

    Add gitignore and api url env

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/', // Replace with your API's base URL
+    baseURL: process.env.VUE_APP_API_URL, // 'http://127.0.0.1:8000/', // Replace with your API's base URL
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
@@ -17,7 +17,7 @@ axiosInstance.interceptors.request.use((config) => {
     if ( authToken ) {
         config.headers.Authorization = `Bearer ${authToken}`;
     }
-    
+
     return config;
 });
 
