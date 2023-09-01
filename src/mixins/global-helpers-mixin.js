8df5a52fe61c7798c709a7365b1130befbd29135
Cristian <cristianald00@hotmail.com>
2023-09-01 16:48:20 -0400

    =Add fltering to timesheets

--- src/mixins/global-helpers-mixin.js
+++ src/mixins/global-helpers-mixin.js
@@ -1,40 +1,64 @@
 export default {
-	components: {
-	},
-	data() {
-		return {
-		}
-	},
-	computed: {
-	},
-	methods: {
-		convertNumberToCurrency(number) {
-			// Convert to number if it's a string
-			const parsedNumber = typeof number === 'string' ? parseFloat(number) : number
+    components: {
+    },
+    data() {
+        return {
+        }
+    },
+    computed: {
+    },
+    methods: {
+        convertNumberToDecimals(number) {
+            let theNumber = number
+            // If is string convert
+            if (typeof theNumber === 'string') {
+                theNumber = parseFloat(theNumber)
+            }
+            if (theNumber) {
+                console.log("NUMBERL : ", theNumber)
+                return theNumber.toFixed(2)
+            }
+        },
+        convertNumberToCurrency(number) {
+            // Convert to number if it's a string
+            const parsedNumber = typeof number === 'string' ? parseFloat(number) : number
 
-			// Check if the parsedNumber is a valid number
-			if (isNaN(parsedNumber)) {
-				return '';
-			}
+            // Check if the parsedNumber is a valid number
+            if (isNaN(parsedNumber)) {
+                return '';
+            }
 
-			// Format the number as currency
-			const currencyFormattedNumber = parsedNumber.toLocaleString('en-US', {
-				style: 'currency',
-				currency: 'USD'
-			});
+            // Format the number as currency
+            const currencyFormattedNumber = parsedNumber.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD'
+            });
 
-			return currencyFormattedNumber;
-		},
-		convertMilitarTimeToNormalTime(time) {
-			const [hours, minutes] = time.split(':').map(Number)
+            return currencyFormattedNumber;
+        },
+        convertMilitarTimeToNormalTime(time) {
+            if (time) {
+                const [hours, minutes] = time.split(':').map(Number)
 
-			if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) {
-				const period = hours >= 12 ? 'PM' : 'AM'
-				const hours12 = (hours % 12) || 12
-				return `${hours12}:${minutes.toString().padStart(2, '0')} ${period}`
-			} else {
-				return 'Invalid time format'
-			}
-		}
-	}
+                if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) {
+                    const period = hours >= 12 ? 'PM' : 'AM'
+                    const hours12 = (hours % 12) || 12
+                    return `${hours12}:${minutes.toString().padStart(2, '0')} ${period}`
+                } else {
+                    return 'Invalid time format'
+                }
+            } else {
+                return ''
+            }
+        },
+        convertDateFormat(dateString) {
+            if (dateString) {
+                const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                const date = new Date(dateString);
+                return date.toLocaleDateString('es-ES', options);
+            } else {
+                return '';
+            }
+        }
+    }
 }
